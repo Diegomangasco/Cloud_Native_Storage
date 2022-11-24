2efd326bd9696438b9cb4c6f9d6e3293a2b6a54f
matteorosani <matteo.rosani@gmail.com>
2022-11-24 10:16:42 +0100

    Rook-Ceph v1.10.0

---
+++ init-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Initializing test environment"
+{
+sudo adduser `id -un` libvirt
+sudo adduser `id -un` kvm
+sudo chmod +x /var/run/libvirt/libvirt-sock
+sudo chown root:libvirt /dev/kvm
+sudo rmmod kvm_intel
+sudo rmmod kvm
+sudo modprobe -a kvm
+sudo modprobe -a kvm_intel
+} &> /dev/null
+
+minikube start --disk-size=8g --extra-disks=1 --driver kvm2 -n 2 --memory 4096 --cpus 2
+
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'complete -F __start_kubectl k' >> ~/.bashrc
+source ~/.bashrc # Sometimes this doesn't work
+
+BASE_URL="https://raw.githubusercontent.com/rook/rook/v1.10.0/deploy/examples/"
+for FILE in "crds.yaml" "common.yaml" "operator.yaml" "cluster-test.yaml" "filesystem-test.yaml" "nfs-test.yaml" "csi/nfs/rbac.yaml" "csi/nfs/storageclass.yaml"
+do
+    kubectl create -f $BASE_URL$FILE
+done
