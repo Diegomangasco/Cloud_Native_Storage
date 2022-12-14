a2d67e806f8522bf4b874eba64f691a14ac4ea81
matteorosani <matteo.rosani@gmail.com>
2022-12-14 16:04:09 +0100

    Added test instance

--- integration-test.sh
+++ integration-test.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
-echo "Enabling kubevirt..."
-minikube addons enable kubevirt
-echo "Waiting for CRDs to deploy..."
-kubectl wait --for condition=established crd/virtualmachines.kubevirt.io &> /dev/null
-echo "kubevirt deployed!"
+echo -n "Enabling kubevirt..."
+minikube addons enable kubevirt &> /dev/null
+kubectl wait --for condition=established crd/virtualmachines.kubevirt.io > /dev/null
+echo "Done!"
 
-wget https://go.dev/dl/go1.16.linux-amd64.tar.gz &> /dev/null
-tar -xf go1.16.linux-amd64.tar.gz &> /dev/null
-sudo rm -rf /usr/local/go
-sudo mv go /usr/local
-sudo apt install -y make gcc
-
-rm go1.16.linux-amd64.tar.gz
+echo -n "Installing go..."
+{
+    wget https://go.dev/dl/go1.16.linux-amd64.tar.gz
+    tar -xf go1.16.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go
+    sudo mv go /usr/local
+    rm go1.16.linux-amd64.tar.gz
+    sudo apt install -y make gcc
+} > /dev/null
 echo "PATH=\$PATH:/usr/local/go/bin" >> $HOME/.bashrc
 PATH=$PATH:/usr/local/go/bin
+echo "Done!"
 
 echo "Installed go version: $(go version)"
 
 BRANCH=${1:-"nfs-storage"}
-echo "Using branch: $BRANCH"
-wget https://github.com/matteorosani/CrownLabs/archive/refs/heads/$BRANCH.tar.gz &> /dev/null
-tar -xf $BRANCH.tar.gz &> /dev/null
-rm $BRANCH.tar.gz
-mv CrownLabs-$BRANCH CrownLabs
+echo -n "Downloading CrownLabs using branch '$BRANCH'"
+{
+    wget https://github.com/matteorosani/CrownLabs/archive/refs/heads/$BRANCH.tar.gz
+    tar -xf $BRANCH.tar.gz
+    rm $BRANCH.tar.gz
+    mv CrownLabs-$BRANCH CrownLabs
+} &> /dev/null
+echo "Done!"
 echo -n "Installing CRDs and operators..."
-( cd CrownLabs/operators && make install )
+( cd CrownLabs/operators && make install > /dev/null)
 echo "Done!"
 echo -n "Starting Instance operator..."
 ( cd CrownLabs/operators && make run-instance &> ~/instance-operator.log ) &
+echo "Done!"
+echo -n "Creating test instance..."
+kubectl create -f https://raw.githubusercontent.com/Diegomangasco/Cloud_Native_Storage/main/instance.yaml > /dev/null
 echo "Done!"
